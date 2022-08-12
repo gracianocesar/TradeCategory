74216c770e9d73fd6521628e44349b726b389558
Cesar Graciano <gracianocesar@gmail.com>
2022-08-12 15:14:00 -0300

    Teste

--- TradeCategory.Lib/PortifolioTrade.cs
+++ TradeCategory.Lib/PortifolioTrade.cs
@@ -26,6 +26,7 @@ namespace TradeCategory.Lib
                         break;
                     }
                 }
+                
             }
             return listCategoryReturn;
         }
